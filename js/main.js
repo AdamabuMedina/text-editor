5acd869d267d30e5a50fef1f65714c1fba9c589b
AdamabuMedina <adamabujamila@gmail.com>
2020-10-28 17:26:33 +0300

    нет решения задачи.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
-if (localStorage.getItem("text-in-editor") !== null) {
-   document.getElementById("editor").innerHTML = localStorage.getItem("text-in-editor")
-}
+var editBtn = document.getElementsByClassName('redactor');
+var saveBtn = document.getElementsByClassName('save');
+var editBtn = document.getElementsByClassName('redactor');
+var editables = document.querySelectorAll('#title, #author, #content')
 
-document.addEventListener('keydown', function (e) {
-   localStorage.setItem('text_in_editor', document.getElementById('editor').innerHTML);
+editBtn.addEventListener('click', function (e) {
+   if (!editables[0].isContentEditable) {
+      editables[0].contentEditable = 'true';
+      editables[1].contentEditable = 'true';
+      editables[2].contentEditable = 'true';
+      saveBtn.addEventListener("click", function () {
+         saveBtn.removeAttr(disabled);
+      }) = 'Save Changes';
+   } else {
+      // выключаем режим редактирования
+      editables[0].contentEditable = 'false';
+      editables[1].contentEditable = 'false';
+      editables[2].contentEditable = 'false';
+      // изменяем текст и цвет кнопки
+      saveBtn.innerHTML = 'Enable Editing';
+      // сохраняем данные в localStorage
+      for (var i = 0; i < editables.length; i++) {
+         localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
+      }
+   }
 });
